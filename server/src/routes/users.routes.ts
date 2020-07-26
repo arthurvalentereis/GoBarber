6abcd62c3c78904bcb3c15ee59479c2dcc1d904d
arthur.valente.reis <arthur.valente.reis@gmail.com>
2020-07-26 03:29:46 -0300

    upload imagem avatar usuário

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,15 +1,12 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../service/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
 
 const usersRouter = Router();
-
-// usersRouter.get('/', async (request, response) => {
-//     const appointmentsRepository = getCustomRepository(AppointmentRepository);
-//     const appointments = await appointmentsRepository.find();
-
-//     response.json(appointments);
-// });
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -28,5 +25,27 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default usersRouter;
