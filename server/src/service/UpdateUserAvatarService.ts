26741c07add16af8b456d8da6e9fa2fc2126807f
arthur.valente.reis <arthur.valente.reis@gmail.com>
2020-08-05 00:31:18 -0300

    Autenticação,  Verificação de Token e Mensagens/Trativas de Erro

--- server/src/service/UpdateUserAvatarService.ts
+++ server/src/service/UpdateUserAvatarService.ts
@@ -1,8 +1,10 @@
+/* eslint-disable camelcase */
 import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
 
 interface Request {
     user_id: string;
@@ -15,9 +17,11 @@ class UpdateUserAvatarService {
         const user = await usersRepository.findOne(user_id);
 
         if (!user) {
-            throw new Error('Only authenticated users can change avatar');
+            throw new AppError(
+                'Only authenticated users can change avatar',
+                401,
+            );
         }
-        console.log(user);
         if (user.avatar) {
             const userAvatarFilePath = path.join(
                 uploadConfig.directory,
