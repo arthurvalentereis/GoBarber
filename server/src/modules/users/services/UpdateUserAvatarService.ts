b0f8105314ea7a90b543677b904b795e9d4bb26a
arthur.valente.reis <arthur.valente.reis@gmail.com>
2020-10-02 20:56:27 -0300

    Finalizado testes.
    Teste para atualização de avatar do usuário

--- server/src/modules/users/services/UpdateUserAvatarService.ts
+++ server/src/modules/users/services/UpdateUserAvatarService.ts
@@ -7,6 +7,7 @@ import {injectable, inject} from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 import IUserRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 
 interface IRequest {
     user_id: string;
@@ -17,7 +18,10 @@ class UpdateUserAvatarService {
 
     constructor (
         @inject('UsersRepository')
-        private usersRepository: IUserRepository
+        private usersRepository: IUserRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider
     ){}
 
     public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -30,20 +34,13 @@ class UpdateUserAvatarService {
             );
         }
         if (user.avatar) {
-            const userAvatarFilePath = path.join(
-                uploadConfig.directory,
-                user.avatar,
-            );
-
-            const userAvatarFileExists = await fs.promises.stat(
-                userAvatarFilePath,
-            );
-
-            if (userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
-        user.avatar = avatarFilename;
+
+        const filename = await this.storageProvider.saveFile(avatarFilename);
+
+        user.avatar = filename;
+
         await this.usersRepository.save(user);
 
         return user;
